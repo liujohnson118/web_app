e501769ee61d7003d86290539fc6e9dff31ee298
liujohnson118 <geyi.liu@outlook.com>
2025-01-02 19:31:43 -0700

    clean up

--- app/services/market_stack_api_requests/dividends_request.rb
+++ app/services/market_stack_api_requests/dividends_request.rb
@@ -1,9 +1,14 @@
 module MarketStackApiRequests
   class DividendsRequest < MarketStackApiRequests::MarketStackApiRequest
-    LIMIT = 0
+    LIMIT = 100
     PATH = "v2/dividends".freeze
 
-    def initialize(symbols: [], limit: 0, offset: 0)
+    # Initializes a new DividendsRequest object.
+    #
+    # @param symbols [Array<String>] an array of stock symbols to request dividends for. Default to empty array.
+    # @param limit [Integer] the maximum number of results to return. Default to 100.
+    # @param offset [Integer] the number of results to skip before starting to collect the result set. Default is 0.
+    def initialize(symbols: [], limit: LIMIT, offset: 0)
       @symbols ||= Array.wrap(symbols)
       @limit ||= limit
       @offset ||= offset
